4625d7058c76ae9bb88531a864f2d096f7070619
unknown <you@example.com>
2020-02-21 17:02:52 +0700

    capnhat6

--- src/main/java/com/code/aeon/AeonApplication.java
+++ src/main/java/com/code/aeon/AeonApplication.java
@@ -1,7 +1,10 @@
 package com.code.aeon;
 
+import com.code.aeon.security.jwt.JwtAuthEntryPoint;
+import com.code.aeon.service.UserDetailsServiceImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AeonApplication {
@@ -9,5 +12,17 @@ public class AeonApplication {
     public static void main(String[] args) {
         SpringApplication.run(AeonApplication.class, args);
     }
+
+    @Bean
+    public UserDetailsServiceImpl userDetailsService() {
+        return new UserDetailsServiceImpl();
+    }
+
+    @Bean
+    public JwtAuthEntryPoint unauthorizedHandler() {
+
+        return new JwtAuthEntryPoint();
+    }
+
 
 }
